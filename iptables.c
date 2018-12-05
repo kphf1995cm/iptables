de5728f9efd5e6f48ce057c0033498826480ec1f
kphf1995cm <kphf1995cm@outlook.com>
2018-12-04 23:53:18 -0800

    forward packets

--- iptables.c
+++ iptables.c
@@ -35,7 +35,7 @@
  *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * nf_router.c - route packets based on the provided config.
+ * forward.c - an example using onvm. Forwards packets to a DST NF.
  ********************************************************************/
 
 #include <unistd.h>
@@ -52,15 +52,11 @@
 #include <rte_common.h>
 #include <rte_mbuf.h>
 #include <rte_ip.h>
-#include <rte_arp.h>
-#include <rte_tcp.h>
-#include <rte_udp.h>
-#include <rte_malloc.h>
 
 #include "onvm_nflib.h"
 #include "onvm_pkt_helper.h"
 
-#define NF_TAG "iptables"
+#define NF_TAG "simple_forward"
 
 /* Struct that contains information about this NF */
 struct onvm_nf_info *nf_info;
@@ -68,9 +64,8 @@ struct onvm_nf_info *nf_info;
 /* number of package between each print */
 static uint32_t print_delay = 1000000;
 
-static uint32_t destination;
 
-static uint8_t log_flag=0; 
+static uint32_t destination;
 
 /*
  * Print a usage message
@@ -80,75 +75,96 @@ usage(const char *progname) {
         printf("Usage: %s [EAL args] -- [NF_LIB args] -- -d <destination> -p <print_delay>\n\n", progname);
 }
 
-
 /*
- * Parse the application arguments. iptables command argument.
+ * Parse the application arguments.
  */
-
 static int
 parse_app_args(int argc, char *argv[], const char *progname) {
+        int c, dst_flag = 0;
+
+        while ((c = getopt(argc, argv, "d:p:")) != -1) {
+                switch (c) {
+                case 'd':
+                        destination = strtoul(optarg, NULL, 10);
+                        dst_flag = 1;
+                        break;
+                case 'p':
+                        print_delay = strtoul(optarg, NULL, 10);
+                        break;
+                case '?':
+                        usage(progname);
+                        if (optopt == 'd')
+                                RTE_LOG(INFO, APP, "Option -%c requires an argument.\n", optopt);
+                        else if (optopt == 'p')
+                                RTE_LOG(INFO, APP, "Option -%c requires an argument.\n", optopt);
+                        else if (isprint(optopt))
+                                RTE_LOG(INFO, APP, "Unknown option `-%c'.\n", optopt);
+                        else
+                                RTE_LOG(INFO, APP, "Unknown option character `\\x%x'.\n", optopt);
+                        return -1;
+                default:
+                        usage(progname);
+                        return -1;
+                }
+        }
+
+        if (!dst_flag) {
+                RTE_LOG(INFO, APP, "Simple Forward NF requires destination flag -d.\n");
+                return -1;
+        }
+
         return optind;
 }
 
 /*
- * Set log flag according ip protocol number
+ * This function displays stats. It uses ANSI terminal codes to clear
+ * screen when called. It is called from a single non-master
+ * thread in the server process, when the process is run with more
+ * than one lcore enabled.
  */
-static void 
-raw(struct rte_mbuf* pkt){
-	
-}
-
 static void
+do_stats_display(struct rte_mbuf* pkt) {
+        const char clr[] = { 27, '[', '2', 'J', '\0' };
+        const char topLeft[] = { 27, '[', '1', ';', '1', 'H', '\0' };
+        static uint64_t pkt_process = 0;
+        struct ipv4_hdr* ip;
+
+        pkt_process += print_delay;
+
+        /* Clear screen and move to top left */
+        printf("%s%s", clr, topLeft);
+
+        printf("PACKETS\n");
+        printf("-----\n");
+        printf("Port : %d\n", pkt->port);
+        printf("Size : %d\n", pkt->pkt_len);
+        printf("N°   : %"PRIu64"\n", pkt_process);
+        printf("\n\n");
+
+        ip = onvm_pkt_ipv4_hdr(pkt);
+        if (ip != NULL) {
+                onvm_pkt_print(pkt);
+        } else {
+                printf("No IP4 header found\n");
+        }
+}
 
 static int
 packet_handler(struct rte_mbuf* pkt, struct onvm_pkt_meta* meta) {
-       
-        struct ether_hdr *eth_hdr;
-        //struct arp_hdr *arp_hdr;
-        struct ipv4_hdr* ip;
-        struct udp_hdr* udp;
-        struct tcp_hdr* tcp;
 
-        ip = onvm_pkt_ipv4_hdr(pkt);
+	struct ipv4_hdr* ip;
 
-        /* If the packet doesn't have an IP header check if its an ARP, if so fwd it to the matched NF */
-        if (ip == NULL) {
-                eth_hdr = onvm_pkt_ether_hdr(pkt);
-                if (rte_cpu_to_be_16(eth_hdr->ether_type) == ETHER_TYPE_ARP) {
-                	meta->destination = destination;
-                    meta->action = ONVM_NF_ACTION_TONF;
-                }
-                // other protocol drop
-                meta->action = ONVM_NF_ACTION_DROP;
-                meta->destination = 0;
-                return 0;
+        static uint32_t counter = 0;
+        if (++counter == print_delay) {
+                do_stats_display(pkt);
+                counter = 0;
         }
-        else{
-        	if(ip->next_proto_id==17){ // udp
 
-        		udp=onvm_pkt_udp_hdr(pkt);
+	ip=onvm_pkt_ipv4_hdr(pkt);
+	printf("src ip:%d\n",ip->srcaddr);
 
-        		// prerouting vs output (srcip)
-
-        		// input vs forward (dstip)
-
-        		// forward
-
-        		//
-
-        	}
-        	else{
-        		if(ip->next_proto_id==6){ // tcp
-        			tcp=onvm_pkt_tcp_hdr(pkt);
-        		}
-        		else{	// other protocol forward
-        			meta->destination = destination;
-                    meta->action = ONVM_NF_ACTION_TONF;
-        		}
-        	}
-        }
-        meta->action = ONVM_NF_ACTION_DROP;
-        meta->destination = 0;
+        meta->action = ONVM_NF_ACTION_TONF;
+        meta->destination = destination;
         return 0;
 }
 
@@ -167,7 +183,6 @@ int main(int argc, char *argv[]) {
                 onvm_nflib_stop();
                 rte_exit(EXIT_FAILURE, "Invalid command-line arguments\n");
         }
-        parse_router_config();
 
         onvm_nflib_run(nf_info, &packet_handler);
         printf("If we reach here, program is ending\n");
